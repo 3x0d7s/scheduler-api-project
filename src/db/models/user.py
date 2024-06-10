1282d94c3d1637b51cde80bb1e2d11f860391cf0
Kryshtof Yurii <yura.krishtof@gmail.com>
2024-06-10 16:16:11 +0300

    changed user model

--- src/db/models/user.py
+++ src/db/models/user.py
@@ -1,3 +1,4 @@
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from sqlalchemy import String
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 
@@ -5,13 +6,7 @@ from src.db.models import TimedBaseModel
 from src.db.models.follow_association import follower_association_table
 
 
-class User(TimedBaseModel):
-    firstname:      Mapped[str] = mapped_column(nullable=False)
-    lastname:       Mapped[str] = mapped_column(nullable=False)
-    username:       Mapped[str] = mapped_column(nullable=False, unique=True)
-    email:          Mapped[str] = mapped_column(nullable=False, unique=True)
-    password:       Mapped[str] = mapped_column(nullable=False)
-
+class User(SQLAlchemyBaseUserTable[int], TimedBaseModel):
     schedules:      Mapped[list["Schedule"]] = relationship(back_populates="user")
     follow_entries: Mapped[list["FollowEntry"]] = relationship(
         secondary=follower_association_table, back_populates="follower")
