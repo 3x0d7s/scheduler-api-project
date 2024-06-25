ae0f7bc52577c5c9e52f98dc314e4836d31c165b
Kryshtof Yurii <yura.krishtof@gmail.com>
2024-06-25 11:40:47 +0300

    added protected-route

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi_users import FastAPIUsers
 
 from src.auth.auth_backend import auth_backend
@@ -26,3 +26,10 @@ app.include_router(
     prefix="/auth",
     tags=["auth"],
 )
+
+current_user = fastapi_users.current_user()
+
+
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.email}"
